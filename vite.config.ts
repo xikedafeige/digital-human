1f604f2ed6b0e52f9628922331ef1c849c68fc52
xikedafeige <39331569+xikedafeige@users.noreply.github.com>
2026-04-23 13:01:24 +0800

    chrom： 数字人-修改本地端口

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 5186,
+    strictPort: true,
+  },
 })
-
